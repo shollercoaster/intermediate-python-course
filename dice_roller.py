94f61a541880cf9ebf96d7ddc515e3c3696260f9
shollercoaster <saumyachat03@gmail.com>
2021-06-26 11:58:10 +0530

    customised outputs

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,17 @@ def main():
     for i in range (0, dice_rolls):
         roll= random.randint(1,6)
         print(f'You rolled a die: {roll}')
+        if roll==1:
+            print("Critical fail!")
+        elif roll==6:
+            print("Critical success!")
+        else:
+            pass
+
         sums+=roll
     print(f"sum of dice rolls: {sums}")
+    
+
 
 if __name__== "__main__":
   main()
